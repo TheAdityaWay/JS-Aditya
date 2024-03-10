e82ee6b77e749a2ee8213f1da84948b5139be598
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-10 11:23:57 +0000

    string

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -2,4 +2,19 @@ const name = "Aditya"
 const repoCount = 5
 
 console.log(name);
-console.log('Hello ${name}');
+//console.log('Hello $ {name}');
+console.log(name +" " + repoCount + " value");
+console.log('Hello my name is ${name} and my RepoCount is ${repoCount}');
+
+const obj = {
+    foo() {
+      return 'bar';
+    },
+  };
+  console.log(obj.foo());
+
+const greeting = '   Hello world!      ';
+console.log(greeting);
+console.log(greeting.trim());
+console.log(greeting.toUpperCase());
+console.log(greeting.length);
