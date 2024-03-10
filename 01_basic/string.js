f24e7ed25c11c5bf640c8dc674e00bf80af160d0
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-10 16:39:44 +0000

    String_2

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -18,3 +18,20 @@ console.log(greeting);
 console.log(greeting.trim());
 console.log(greeting.toUpperCase());
 console.log(greeting.length);
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]);
+const chars = str.split('');
+console.log(chars[8]);
+const strCopy = str.split();
+console.log(strCopy);
+
+//const index = 4;
+//console.log('The character at index ${index} is ${sentence.charAt(index)}');
+
+let index = 5;
+console.log(`An index of ${index} returns the character ${str.at(index)}`);
+index = -4;
+console.log(`An index of ${index} returns the character ${str.at(index)}`);
