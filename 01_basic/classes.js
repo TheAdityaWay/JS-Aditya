026aa6d8cd173215892f21517a42e678220a1809
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-13 16:12:07 +0000

    classes2

--- 01_basic/classes.js
+++ 01_basic/classes.js
@@ -1,10 +1,32 @@
-class Person {
-    name;
-    constructor(name) {
-      this.name = name;
-    }
-    introduceSelf() {
-      console.log("Hi! I'm ${this.name}");
-    }
+//class Person {
+//    name;
+//    constructor(name) {
+//      this.name = name;
+//    }
+//    introduceSelf() {
+//      console.log("Hi! I'm ${this.name}");
+//    }
+//  }
+
+/*class Animal {
+  sleep() {
+    console.log("zzzzzzz");
   }
-  
+}*/
+
+class Example {
+  somePublicMethod() {
+    this.#somePrivateMethod();
+  }
+
+  #somePrivateMethod() {
+    console.log("You called me?");
+  }
+}
+
+const myExample = new Example();
+
+myExample.somePublicMethod(); // 'You called me?'
+
+//myExample.#somePrivateMethod(); // SyntaxError
+
