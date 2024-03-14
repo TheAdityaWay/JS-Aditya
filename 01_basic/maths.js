c3f54c538e3969a6d98ea887ebec27dfdb357302
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-14 06:40:11 +0000

    maths

---
+++ 01_basic/maths.js
@@ -0,0 +1,20 @@
+/*
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+
+const otherNumber = 23.456
+console.log(otherNumber.toPrecision(3));
+*/
+
+//+++++++++++++++++++Maths++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.9));
+
+console.log(Math.min(4,5,6,3,2,));
+console.log(Math.max(4,5,6,3,2,));
