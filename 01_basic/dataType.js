d8c5debb5f889d5f34fd5b206be81c6012105be0
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-15 17:54:13 +0000

    minor

--- 01_basic/dataType.js
+++ 01_basic/dataType.js
@@ -18,3 +18,16 @@ console.log(typeof null);
 
 console.log(score);
 console.log(typeof score);
+
+async function populate() {
+    const requestURL =
+      "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+  
+    populateHeader(superHeroes);
+    populateHeroes(superHeroes);
+  }
+  
