3c370f7f32dcfe13f59981e1ed80aad0ed1bf0fe
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-03-20 17:50:37 +0000

    .

--- 01_basic/arrays.js
+++ 01_basic/arrays.js
@@ -36,4 +36,9 @@ console.log(myn2);
 
 console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from({name: "hitesh"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
